ce655243a406dce1026db1229df4fbd11e2b413a
Aaron Smith <aaron@web-presence.biz>
2022-08-01 07:55:06 -0700

    blog one at a time

--- js/navigation.js
+++ js/navigation.js
@@ -7,10 +7,17 @@ function showModule(module) {
         e.classList.remove("active");
     });
 
+
     document.querySelector("[data-portfolio='" + module + "']").classList.add("active");
     document.querySelector("[data-module='" + module + "']").classList.remove("hide");
     document.querySelector("[data-module='" + module + "']").classList.add("fadeIn");
     localStorage.setItem("activeModule", module);
+
+    if (module === "blog") {
+        document.getElementById("blogButtons").classList.remove("hide");
+    } else {
+        document.getElementById("blogButtons").classList.add("hide");
+    }
 }
 
 if (localStorage.getItem("activeModule")) {
