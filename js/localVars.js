385f409f489152e8d724f840062f87b0a8358653
Aaron Smith <aaron@web-presence.biz>
2024-11-25 16:17:22 -0700

    adding new post about js frameworks

--- js/localVars.js
+++ js/localVars.js
@@ -2,6 +2,7 @@ const localVars = [
     {
 
         blogUrls: [
+            "/2024/11/front-end-framework-without-react-or.html",
             "/2024/11/reactjs-build-dynamic-hinting-feature.html",
             "/2024/10/javascript-tutorial-hijack-js-datepicker.html",
             "/2024/05/jwt-login-framework-2024-server-side.html",
