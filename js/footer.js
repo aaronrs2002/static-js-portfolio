3125e8cf363573b8ade924b65f3ede6f55b95ba8
Aaron Smith <aaron@web-presence.biz>
2025-02-12 08:00:42 -0700

    new titles for icons

--- js/footer.js
+++ js/footer.js
@@ -1,10 +1,10 @@
 const iconLinks = [
-    { title: "linkedin", target: "_blank", url: "https://www.linkedin.com/in/aaronrs2002", iconClass: "fab fa-linkedin" },
-    { title: "github", target: "_blank", url: "https://github.com/aaronrs2002", iconClass: "fab fa-github" },
-    { title: "resume", target: "_blank", url: "downloads/Resume-Aaron-Smith.pdf", iconClass: "fas fa-file-pdf" },
-    { title: "ticket management and accounting software by: Aaron Smith ", target: "_blank", url: "https://aaronrs2002.github.io/task-master/", iconClass: "fas fa-network-wired" },
-    { title: "youtube", target: "_blank", url: "https://www.youtube.com/@web-presence-developer", iconClass: "fab fa-youtube" },
-    { title: "vanilla-javascript-games by: Aaron Smith", target: "_blank", url: "https://aaronrs2002.github.io/black-jack/", iconClass: "fas fa-dice" }];
+    { title: "My Linkedin Profile", target: "_blank", url: "https://www.linkedin.com/in/aaronrs2002", iconClass: "fab fa-linkedin" },
+    { title: "My Open Source Library", target: "_blank", url: "https://github.com/aaronrs2002", iconClass: "fab fa-github" },
+    { title: "My Resume", target: "_blank", url: "downloads/Resume-Aaron-Smith.pdf", iconClass: "fas fa-file-pdf" },
+    { title: "Networked serverless web apps for Workflow Management and Accounting", target: "_blank", url: "https://aaronrs2002.github.io/task-master/", iconClass: "fas fa-network-wired" },
+    { title: "Learn how to write web applications!", target: "_blank", url: "https://www.youtube.com/@web-presence-developer", iconClass: "fab fa-youtube" },
+    { title: "Blackjack, Texas Hold'em, Slot Machine, Bingo and Trivia - No Install. No Login.", target: "_blank", url: "https://aaronrs2002.github.io/black-jack/", iconClass: "fas fa-dice" }];
 
 let footerIconsHTML = "";
 let landingPgIconsHTML = "";
