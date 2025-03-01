a6b2de566424f51e025b6b22ee6d7885cd2839fc
Aaron Smith <aaron@web-presence.biz>
2025-02-28 17:08:38 -0700

    new post

--- js/localVars.js
+++ js/localVars.js
@@ -2,6 +2,7 @@ const localVars = [
     {
 
         blogUrls: [
+            "/2025/02/javascript-serverless-web-apps-for-task.html",
             "/2025/02/javascript-finance-analysis-app-budget.html",
             "/2025/01/build-budgeting-app-with-javascript.html",
             "/2025/01/manage-tasks-time-tracking-for.html",
