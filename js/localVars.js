6712d1656b38f785c1cc462ec0779251d1a135eb
Aaron Smith <aaron@web-presence.biz>
2025-05-07 13:24:36 -0700

    nw post restaurant template intro

--- js/localVars.js
+++ js/localVars.js
@@ -2,6 +2,7 @@ const localVars = [
     {
 
         blogUrls: [
+            "/2025/05/restaurant-website-template-serverless.html",
             "/2025/04/rss-feed-reader-without-ads-javascript.html",
             "/2025/03/free-no-login-crapsdice-game-built-with.html",
             "/2025/02/javascript-serverless-web-apps-for-task.html",
