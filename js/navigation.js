366c1e7821f1ed617e65a51e61fe433fdf1c90f7
Aaron Smith <aaron@web-presence.biz>
2025-09-08 15:56:41 -0700

    re-targeted my resume text no more modal2

--- js/navigation.js
+++ js/navigation.js
@@ -33,7 +33,13 @@ function showModule(module) {
     }
 
     [].forEach.call(document.querySelectorAll("[data-module]"), function (e) {
-        e.classList.add("hide");
+        if (e.dataset.module === module) {
+            e.classList.remove("hide");
+
+        } else {
+            e.classList.add("hide");
+        }
+
     });
 
     [].forEach.call(document.querySelectorAll("[data-portfolio]"), function (e) {
@@ -41,7 +47,8 @@ function showModule(module) {
     });
 
     document.querySelector("[data-portfolio='" + module + "']").classList.add("active");
-    document.querySelector("[data-module='" + module + "']").classList.remove("hide");
+
+    // document.querySelector("[data-module='" + module + "']").classList.remove("hide");
     document.querySelector("[data-module='" + module + "']").classList.add("fadeIn");
     localStorage.setItem("activeModule", module);
 
